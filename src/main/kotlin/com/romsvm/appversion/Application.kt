a1d68c6babf7df84da09b0277bec89da7cec678c
Roman Mityukov <roman.mityukov@actonica.ru>
2018-04-21 05:33:58 +0700

    creating war without embedded tomcat

--- src/main/kotlin/com/romsvm/appversion/Application.kt
+++ src/main/kotlin/com/romsvm/appversion/Application.kt
@@ -1,11 +1,20 @@
 package com.romsvm.appversion
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class Application
+class Application : SpringBootServletInitializer() {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<Application>(*args)
+        }
+    }
 
-fun main(args: Array<String>) {
-    runApplication<Application>(*args)
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder.sources(Application::class.java)
+    }
 }
