983106d616f10c1cce5c910dc4404fffc1af8b3e
Roman Mityukov <roman.mityukov@actonica.ru>
2018-04-25 20:10:33 +0700

    change controller mapping

--- src/main/kotlin/com/romsvm/appversion/AppVersionController.kt
+++ src/main/kotlin/com/romsvm/appversion/AppVersionController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*
 import java.util.Optional
 
 @RestController
-@RequestMapping(path= arrayOf("/appversion"))
+@RequestMapping(path= arrayOf(""))
 class AppVersionController {
     @Autowired
     lateinit var versionRepository: VersionRepository
